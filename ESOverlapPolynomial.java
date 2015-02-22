9833f8e00b80490cf87d1d413b4aed95cfdb6e17
Psychonut <weikanglim@gmail.com>
2015-02-22 15:29:35 -0600

    Testing solving roots as polynomial.

--- ESOverlapPolynomial.java
+++ ESOverlapPolynomial.java
@@ -8,14 +8,13 @@ package org.opensourcephysics.sip.CPM;
  * @author Wei Kang Lim
  *
  */
-public class ESOverlapPolynomial implements org.opensourcephysics.numerics.Function {
+public class ESOverlapPolynomial extends org.opensourcephysics.numerics.Polynomial {
 	private double l1,l2,l3; // eigenvalues
 	private double x0,y0,z0; // coordinate of sphere
-	
-	@SuppressWarnings("unused")
-	private ESOverlapPolynomial(){} // hide constructor
-	
-	
+	public ESOverlapPolynomial(double [] coefficients){
+		super(coefficients);
+	}
+
 	/**
 	 * Instantiates the function with the required variables.
 	 * @param ellips double[] An array containing the ellipsoid radii squared.<br>
@@ -24,7 +23,8 @@ public class ESOverlapPolynomial implements org.opensourcephysics.numerics.Funct
 	 * 				 [0,1,2] = The sphere coordinates, relative to the center of ellipsoid. <br>
 	 */
 	public ESOverlapPolynomial(double [] ellips, double [] sphere){
-		update(ellips, sphere);
+		this(new double [7]); // Creates polynomial with all coefficients equal to zero.
+		update(ellips, sphere); // Updates the polynomail coefficients.
 	}
 	
 	/**
@@ -37,38 +37,22 @@ public class ESOverlapPolynomial implements org.opensourcephysics.numerics.Funct
 	public void update(double [] ellips, double [] sphere){
 		l1 = ellips[0]; l2=ellips[1]; l3=ellips[2];
 		x0 = sphere[0]; y0 = sphere[1]; z0=sphere[2];
+		updateCoefficients();
 	}
 	
-	
-	/**
-	 * Evaluates the polynomial at a given point.
-	 */
-	public double evaluate(double x){
+	private void updateCoefficients(){
 		double a = l2/(l1*l1) * y0*y0;
 		double b = l2/l1 - 1;
 		double c = l3/(l1*l1) * z0*z0;
 		double d = l3/l1 - 1;
 		double e = this.x0*this.x0/l1;
-		double u = this.x0 / x;
-		
-		 return e/(u*u) + a/((u+b)*(u+b)) + c/((u+d)*(u+d)) - 1;
-//		return e*(u+b)*(u+b)*(u+d)*(u+d) + a*u*u*(u+d)*(u+d) + c*u*u*(u+b)*(u+b) - u*u*(u+b)*(u+b)*(u+d)*(u+d);
-	}
-	
-	public String toString(){
-		double a = l2/(l1*l1) * y0*y0;
-		double b = l2/l1 - 1;
-		double c = l3/(l1*l1) * z0*z0;
-		double d = l3/l1 - 1;
-		
-		String result = "x^2/$l1+$a/($x0/x+$b)^2+$c/($x0/x+$d)^2";
-		result = result.replace("$l1", Double.toString(l1));
-		result = result.replace("$x0", Double.toString(x0));
-		result = result.replace("$a", Double.toString(a));
-		result = result.replace("$b", Double.toString(b));
-		result = result.replace("$c", Double.toString(c));
-		result = result.replace("$d", Double.toString(d));
-		
-		return result;
+
+		coefficients[6]=Math.pow(b,2)*Math.pow(d,2)*e;
+		coefficients[5]=2*Math.pow(b,2)*d*e*x0+2*b*Math.pow(d,2)*e*x0;
+		coefficients[4]=Math.pow(b,2)*c*Math.pow(x0,2)+a*Math.pow(d,2)*Math.pow(x0,2)-Math.pow(b,2)*Math.pow(d,2)*Math.pow(x0,2)+Math.pow(b,2)*e*Math.pow(x0,2)+4*b*d*e*Math.pow(x0,2)+Math.pow(d,2)*e*Math.pow(x0,2);
+		coefficients[3]=2*b*c*Math.pow(x0,3)+2*a*d*Math.pow(x0,3)-2*Math.pow(b,2)*d*Math.pow(x0,3)-2*b*Math.pow(d,2)*Math.pow(x0,3)+2*b*e*Math.pow(x0,3)+2*d*e*Math.pow(x0,3);
+		coefficients[2]=a*Math.pow(x0,4)-Math.pow(b,2)*Math.pow(x0,4)+c*Math.pow(x0,4)-4*b*d*Math.pow(x0,4)-Math.pow(d,2)*Math.pow(x0,4)+e*Math.pow(x0,4);
+		coefficients[1]=-2*b*Math.pow(x0,5)-2*d*Math.pow(x0,5);
+		coefficients[0]=-Math.pow(x0,6);
 	}
 }
